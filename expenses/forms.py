b99dcb5ba13a4bd849891851edf3b3b91a162427
WscieklyDunczyk <sn.nowak99@gmail.com>
2023-01-15 22:52:45 +0100

    v.0.0.2

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,8 +1,18 @@
 from django import forms
-from .models import Expense
+from .models import Expense, Category
 
 
 class ExpenseSearchForm(forms.ModelForm):
+    datefrom = forms.DateTimeField(required=False, label="Date from")
+    dateto = forms.DateTimeField(required=False, label="Date to")
+    multiplecategories = forms.ModelMultipleChoiceField(queryset=Category.objects.all(),
+                                                        label="Category", widget=forms.CheckboxSelectMultiple,
+                                                        required=False)
+    SORTING = [('ascending', 'Ascending'), ('descending', 'Descending')]
+    sorting_order = forms.ChoiceField(required=False, widget=forms.RadioSelect, choices=SORTING)
+    SORTING_BY = [('category', 'Category'), ('date', 'Date')]
+    sorting_category = forms.ChoiceField(required=False, widget=forms.RadioSelect, choices=SORTING_BY)
+
     class Meta:
         model = Expense
         fields = ('name',)
@@ -10,3 +20,4 @@ class ExpenseSearchForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].required = False
+
--- expenses/views.py
+++ expenses/views.py
@@ -1,5 +1,5 @@
-from django.views.generic.list import ListView
-
+from django.views.generic import ListView, UpdateView
+from django.db.models import Sum
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
 from .reports import summary_per_category
@@ -15,16 +15,49 @@ class ExpenseListView(ListView):
         form = ExpenseSearchForm(self.request.GET)
         if form.is_valid():
             name = form.cleaned_data.get('name', '').strip()
+            datefrom = form.cleaned_data.get('date1')
+            dateto = form.cleaned_data.get('date2')
+            multiplecategories = form.cleaned_data.get('multiplecategories')
+            sorting_order = form.cleaned_data.get('sorting_order')
+            sorting_by = form.cleaned_data.get('sorting_category')
+
             if name:
                 queryset = queryset.filter(name__icontains=name)
+            if datefrom or dateto:
+                queryset = queryset.filter(date__range=[datefrom, dateto])
+            if multiplecategories:
+                queryset = queryset.filter(category__in=multiplecategories)
+
+            if sorting_by == '':
+                sorting_by = 'category'
+
+            if sorting_order == 'ascending':
+                queryset = queryset.order_by(sorting_by)
+            else:
+                sorting_by = '-' + sorting_by
+                queryset = queryset.order_by(sorting_by)
 
         return super().get_context_data(
             form=form,
             object_list=queryset,
             summary_per_category=summary_per_category(queryset),
+            total_amount=queryset.aggregate(Sum('amount')),
+            summary_per_year=queryset.all().values_list('date__year').annotate(Sum('amount')).order_by('date__year'),
+            summary_per_month=queryset.all().values_list('date__month').annotate(Sum('amount')).order_by('date__month'),
             **kwargs)
+
 
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
+
+    def get_context_data(self):
+        expenses_per_category = Expense.objects.values_list('category__name', 'category__id').annotate(Sum('amount'))
+
+        return super().get_context_data(expenses_per_category=expenses_per_category)
+
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = ['name']
 
